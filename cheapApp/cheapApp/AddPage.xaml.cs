7f68a6b43f8a70a84ab47b9c4dcd80c6400c9359
jakkrapun <sirjakk@gmail.com>
2016-08-24 15:57:19 -0700

    first commit

---
+++ cheapApp/cheapApp/AddPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace cheapApp
+{
+    public partial class AddPage : ContentPage
+    {
+        public AddPage()
+        {
+            InitializeComponent();
+        }
+
+        async void CommitProduct(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ cheapApp/cheapApp/ShowListPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace cheapApp
+{
+    public partial class ShowListPage : ContentPage
+    {
+        public ShowListPage()
+        {
+            InitializeComponent();
+        }
+
+        async void AddPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddPage());
+            //await Navigation.PopAsync();
+        }
+    }
+}
